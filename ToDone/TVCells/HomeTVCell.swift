67ec770470256f5cfc74a3e6d62e55393f007645
Soumyajit Pal <soumyajitpal810@gmail.com>
2023-12-08 01:22:59 +0530

    Initial Comit

---
+++ ToDone/TVCells/HomeTVCell.swift
@@ -0,0 +1,25 @@
+//
+//  HomeTVCell.swift
+//  ToDone
+//
+//  Created by Soumyajit Pal on 07/12/23.
+//
+
+import UIKit
+
+class HomeTVCell: UITableViewCell {
+
+    @IBOutlet weak var HomeTVCellLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
